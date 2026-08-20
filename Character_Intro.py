b996fe76ebe0002177e8fe3eb8d8fb624b9955bb
hugocarney-arch <hugo.carney@ucas-edu.net>
2026-08-20 09:42:15 -0600

    Finished assignment Character Intro

---
+++ Character_Intro.py
@@ -0,0 +1,7 @@
+# Hugo Carney, Character_Intro.py Assignment
+
+name = "Billy"
+age = "17"
+job = "Programmer"
+birth_place = "Utah"
+print("This is " + name + " he is "+ age + " and is a " + job + " and he was born in " + birth_place + ".")
