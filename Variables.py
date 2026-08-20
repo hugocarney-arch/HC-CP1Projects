d6c403e6f183ae299f141817a222c4914d963284
hugocarney-arch <hugo.carney@ucas-edu.net>
2026-08-20 09:07:16 -0600

    Made Variables Notes Code

---
+++ Variables.py
@@ -0,0 +1,19 @@
+# Hugo Carney, Variable Notes
+
+# Create a variable
+name = "Katie"
+
+print("Hello " + name)
+name = "Alex"
+
+print("Sorry I meant " + name)
+
+
+
+# Another Variable Example
+age = 100
+
+print("wow you are", age)
+print("Oh wait your birthday was yesterday?")
+age += 1
+print("so you are", age)
