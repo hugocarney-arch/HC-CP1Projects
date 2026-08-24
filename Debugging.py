bd2381423f586cc1dc4fbe62439060169a68eb90
hugocarney-arch <hugo.carney@ucas-edu.net>
2026-08-24 09:10:45 -0600

    Did assignment /debugged it

---
+++ Debugging.py
@@ -0,0 +1,5 @@
+name = "Alex"
+age = 16
+#                                                           Added end parenthese
+print("My name is " + name + " and I am " + str(age) + " years old.")
+#Added e to nam to make it name which is the variables correct name
