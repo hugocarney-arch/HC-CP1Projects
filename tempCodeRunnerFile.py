30d185258b360245d233e923c51e209c33696403
hugocarney-arch <hugo.carney@ucas-edu.net>
2026-09-09 09:04:15 -0600

    Deleted Code

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-while True:
-    try:
-        gpa = float(input("What is your GPA: "))
-        break
-    except ValueError:
-        print("Not a valid GPA. Please enter a number.")
-
-fixed_number = input("Please enter your 10-digit phone number: ").strip()
