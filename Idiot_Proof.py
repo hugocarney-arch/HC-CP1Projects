d867dacc3bad348486a169f887ee575fae589432
hugocarney-arch <hugo.carney@ucas-edu.net>
2026-09-09 11:56:16 -0600

    fixed

--- Idiot_Proof.py
+++ Idiot_Proof.py
@@ -5,8 +5,9 @@ last_name = input("Okay what about your last: ").strip().capitalize()
 full_name = (f" {first_name} {last_name} ")
 print("Okay")
 
-fixed_number = input("Please enter your 10-digit phone number: ").strip()
+
 while True:
+    fixed_number = input("Please enter your 10-digit phone number: ").strip()
     if fixed_number.isdigit() and len(fixed_number) == 10:
         phone_number = f"{fixed_number[:3]} {fixed_number[3:6]} {fixed_number[6:]}"
         break
