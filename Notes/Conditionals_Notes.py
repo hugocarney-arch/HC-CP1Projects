909da0a4417932d5aff37f11b9d5c4072cd6d904
hugocarney-arch <hugo.carney@ucas-edu.net>
2026-09-24 09:55:17 -0600

    Started User sign in Assignmnet

---
+++ Notes/Conditionals_Notes.py
@@ -0,0 +1,18 @@
+# HC 1st Conditional Notes
+grade = 64
+
+if grade >= 90:
+    print("You hvae an A good job! ")
+elif grade >= 70:
+    print("You are passing! ")
+else:
+    print("You are not passing! ")
+    print("Do you need to retake a quiz or do you need to submit a missing assignment ")
+
+
+raining = False
+
+if raining:
+    print("Bring a umbrella ")
+else:
+    print("Wear sunscreen ")
---
+++ User_Signin.py
@@ -0,0 +1,12 @@
+# HC 1st User Sign In
+
+user_name = "Skibidi Sigma"
+password = 12345
+
+while True:
+    user_name = input("Please enter your User Name: ")
+    password = input("Please enter your Password: ")
+    if user_name == "Skibidi Sigma" and password == 12345:
+        print("Welcome in to the secret online gambiling leaque!")
+    else:
+        print("Sorry invalid credentials plese renter password and user name make sure to use correct capitilization") 
