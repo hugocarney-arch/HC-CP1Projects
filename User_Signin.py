c42ab9b1512d2d24521b534a9279d0082e52f363
hugocarney-arch <hugo.carney@ucas-edu.net>
2026-09-24 15:57:20 -0600

    Fixed Assignment

--- User_Signin.py
+++ User_Signin.py
@@ -6,7 +6,7 @@ while True:
     user_name = input("Please enter your User Name: ")
     password = input("Please enter your Password: ")
     if user_name == correct_user_name and password == correct_password:
-        print(f"Welcome in to the secret online gambiling leaque {gender} {last_name}")
+        print(f"Welcome in to the secret online gambiling")
         break
     else:
         print("Sorry invalid credentials plese renter password and user name make sure to use correct capitilization") 
